13fec863188247c707e3e2738da80692b0af9f3b
Jeronimo Avila <ingejeroavila715@gmail.com>
2025-12-30 16:11:06 -0500

    Add files via upload

---
+++ HolaMundo.java
@@ -0,0 +1,29 @@
+
+package holamundo;
+
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hola Mundo");
+ // operacion de asignacion       
+        int edad = 21;                    
+        double estatura = 1.86;
+        boolean tieneCarnet = true;
+        char letra = 'J';
+        String direccion = "calle 15 # 14-35";
+        long numeroLargo = 35165464;
+        
+        System.out.println("Hola mi edad es:" + edad);
+        System.out.println("Mi estatura es:" + estatura);
+        System.out.println("Direccion:" + direccion);
+      
+         
+        
+        
+          
+        
+     
+        
+    }   
+}
